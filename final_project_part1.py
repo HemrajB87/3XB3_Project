961a3461894432a610d07743971983e2b3d42393
Hemraj <bhatth14@mcmaster.ca>
2023-12-02 22:31:44 -0500

    Part 1 Bellman_ford and Dijkstra with k parameter

--- final_project_part1.py
+++ final_project_part1.py
@@ -1,4 +1,8 @@
 import min_heap2 as min_heap
+import matplotlib
+matplotlib.use('TkAgg')  # Use the TkAgg backend (or another backend that works for you)
+import matplotlib.pyplot as plt
+import time
 import random
 
 class DirectedWeightedGraph:
@@ -119,23 +123,32 @@ def init_d(G):
 def dijkstra_approx(G, source, k):
     pred = {}
     dist = {}
+    relax_count = {}
     Q = min_heap.MinHeap([])
     nodes = list(G.adj.keys())
 
     for node in nodes:
         Q.insert(min_heap.Element(node, float("inf")))
         dist[node] = float("inf")
+        relax_count[node] = 0
     Q.decrease_key(source, 0)
 
     while not Q.is_empty():
         current_element = Q.extract_min()
         current_node = current_element.value
         dist[current_node] = current_element.key
+        relax_count[current_node] += 1
+
+        if relax_count[current_node] > k:
+            continue  # Skip relaxing this node further
+
         for neighbour in G.adj[current_node]:
-            if dist[current_node] + G.w(current_node, neighbour) < dist[neighbour] and pred.get(neighbour, -1) != current_node and dist[current_node] < k:
+            if dist[current_node] + G.w(current_node, neighbour) < dist[neighbour]:
                 Q.decrease_key(neighbour, dist[current_node] + G.w(current_node, neighbour))
                 dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
                 pred[neighbour] = current_node
+                relax_count[neighbour] += 1
+
     return dist
 
 # Part 1
@@ -151,7 +164,9 @@ def bellman_ford_approx(G, source, k):
     for _ in range(k):
         for node in nodes:
             for neighbour in G.adj[node]:
-                if dist[neighbour] > dist[node] + G.w(node, neighbour) and dist[node] < k:
+                if dist[neighbour] > dist[node] + G.w(node, neighbour):
                     dist[neighbour] = dist[node] + G.w(node, neighbour)
                     pred[neighbour] = node
     return dist
+
+
